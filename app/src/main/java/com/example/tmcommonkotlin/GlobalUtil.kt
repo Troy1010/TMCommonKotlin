35fd6cfe6fe4da35645668fba7d8e78c64d8b882
Troy Swanson <2troy2020@gmail.com>
2020-06-03 12:23:45 -0500

    v1.0.10`easySnackbar, tryPermissibleAction

--- app/src/main/java/com/example/tmcommonkotlin/GlobalUtil.kt
+++ app/src/main/java/com/example/tmcommonkotlin/GlobalUtil.kt
@@ -2,7 +2,10 @@ package com.example.tmcommonkotlin
 
 import android.content.Context
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.snackbar.Snackbar
 
 fun logz (msg:String) {
     Log.d("TMLog", "TM`$msg")
@@ -10,6 +13,20 @@ fun logz (msg:String) {
 
 fun easyToast(context: Context, msg: String, lengthID:Int=Toast.LENGTH_SHORT) {
     Toast.makeText(context, msg, lengthID).show()
+}
+
+fun easySnackbar(
+    coordinatorLayout: CoordinatorLayout,
+    msg: String,
+    actionText: String? = null,
+    action: View.OnClickListener? = null
+) {
+    var sb = Snackbar
+        .make(coordinatorLayout, msg, Snackbar.LENGTH_LONG)
+    if ((actionText != null) and (action != null)) {
+        sb.setAction(actionText, action)
+    }
+    sb.show()
 }
 
 //fun invokeHiddenMethod(name: String) {
--- app/src/main/java/com/example/tmcommonkotlin/inheritables/TMActivity.kt
+++ app/src/main/java/com/example/tmcommonkotlin/inheritables/TMActivity.kt
@@ -6,12 +6,11 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.tmcommonkotlin.CAMERA_REQUEST_CODE
-import com.example.tmcommonkotlin.easyToast
-import com.example.tmcommonkotlin.logz
+import com.example.tmcommonkotlin.*
 
 abstract class TMActivity(val layout: Int?=null, val theme: Int? = null) :
     AppCompatActivity() {
@@ -22,22 +21,24 @@ abstract class TMActivity(val layout: Int?=null, val theme: Int? = null) :
         layout?.run { setContentView(layout) }
     }
 
-    // helps easyPhoto
+    // helps tryPermissibleAction
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<out String>, grantResults: IntArray
     ) {
-        when (requestCode) {
-            CAMERA_REQUEST_CODE -> {
-                if ((!grantResults.isEmpty()) and (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    takePhoto()
+        for (permissibleAction in permissibleActions.values) {
+            if (permissibleAction.code == requestCode) {
+                if ((!grantResults.isEmpty()) and hasAllPermissionsGranted(grantResults)) {
+                    permissibleAction.action()
                 } else {
-                    easyToast(this, "Action requires Camera permissions")
+                    easyToast(this, "Action requires permissions", Toast.LENGTH_LONG)
                 }
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
-    private fun takePhoto() {
+
+
+    val takePhoto = {
         startActivityForResult(
             Intent(MediaStore.ACTION_IMAGE_CAPTURE),
             CAMERA_REQUEST_CODE
@@ -45,18 +46,25 @@ abstract class TMActivity(val layout: Int?=null, val theme: Int? = null) :
     }
 
     fun easyPhoto() {
-        // *Remember Manifest: <uses-permission android:name="android.permission.CAMERA"/>
-        val permission = ContextCompat.checkSelfPermission(
-            this, Manifest.permission.CAMERA
+        tryPermissibleAction(this,
+        PermissibleAction(arrayOf(
+            Manifest.permission.CAMERA
+        ),
+        CAMERA_REQUEST_CODE,
+        takePhoto)
         )
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.CAMERA),
-                CAMERA_REQUEST_CODE
-            )
-        } else {
-            takePhoto()
-        }
+        // *Remember Manifest: <uses-permission android:name="android.permission.CAMERA"/>
+//        val permission = ContextCompat.checkSelfPermission(
+//            this, Manifest.permission.CAMERA
+//        )
+//        if (permission != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(
+//                this,
+//                arrayOf(Manifest.permission.CAMERA),
+//                CAMERA_REQUEST_CODE
+//            )
+//        } else {
+//            takePhoto()
+//        }
     }
 }
