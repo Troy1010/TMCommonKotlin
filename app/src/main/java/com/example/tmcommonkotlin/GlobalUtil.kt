7f4208be2f8c63486774c8383f935d0063b63c56
Troy Swanson <2troy2020@gmail.com>
2020-06-06 18:54:05 -0500

    GlobalUtil`getImageUri

--- app/src/main/java/com/example/tmcommonkotlin/GlobalUtil.kt
+++ app/src/main/java/com/example/tmcommonkotlin/GlobalUtil.kt
@@ -1,11 +1,15 @@
 package com.example.tmcommonkotlin
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.snackbar.Snackbar
+import java.io.ByteArrayOutputStream
 
 fun logz (msg:String) {
     Log.d("TMLog", "TM`$msg")
@@ -34,3 +38,11 @@ fun easySnackbar(
 //    method.isAccessible = true
 //    method.invoke(testSubject)
 //}
+
+fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
+    val bytes = ByteArrayOutputStream()
+    inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+    val path =
+        MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
+    return Uri.parse(path)
+}
