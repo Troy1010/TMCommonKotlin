71f7b07ff9f7ce39bc5e67d26eb36bbdda7748f2
Troy Swanson <2troy2020@gmail.com>
2020-06-16 18:28:19 -0500

    ReactiveX logging

--- app/src/main/java/com/example/tmcommonkotlin/ReactiveX.kt
+++ app/src/main/java/com/example/tmcommonkotlin/ReactiveX.kt
@@ -3,19 +3,23 @@ package com.example.tmcommonkotlin
 import android.annotation.SuppressLint
 import io.reactivex.Observable
 
-@SuppressLint("CheckResult")
-fun <T> Observable<T>.logO(msgPrefix: String? = null): Observable<T> {
-    val tempMsgPrefix: String = if (msgPrefix == null) "" else { "$msgPrefix`" }
-    return this.doOnNext { logz("$tempMsgPrefix$it") }
-}
+//// very simple version
+//fun <T> Observable<T>.logO(msgPrefix: String? = null): Observable<T> {
+//    val tempMsgPrefix: String = if (msgPrefix == null) "" else {
+//        "$msgPrefix`"
+//    }
+//    return this.doOnNext { logz("$tempMsgPrefix$it") }
+//}
 
-@SuppressLint("CheckResult")
-fun <T> Observable<T>.log(msgPrefix:String? = null, bType:Boolean=false) : Observable<T> {
-    val tempMsgPrefix : String = if (msgPrefix==null) "" else { "$msgPrefix`" }
+// bType prints the type
+fun <T> Observable<T>.log(msgPrefix: String? = null, bType: Boolean = false): Observable<T> {
+    val tempMsgPrefix: String = if (msgPrefix == null) "" else {
+        "$msgPrefix`"
+    }
     return this
         .doOnNext {
             if (bType) {
-                val typeName = if (it==null) {
+                val typeName = if (it == null) {
                     "null"
                 } else {
                     (it as Any)::class.java.simpleName
@@ -25,14 +29,40 @@ fun <T> Observable<T>.log(msgPrefix:String? = null, bType:Boolean=false) : Obser
                 logz("$tempMsgPrefix$it")
             }
         }
+        .doOnError {
+            logz("${tempMsgPrefix}Error`$it")
+        }
 }
 
 
 val TMAlphabet = "abcdefghijklmnopqrstuvwxyz"
 var TMRXCounter = 0
-fun <T> Observable<T>.loga(msgPrefix:String? = null, bType:Boolean=false) : Observable<T> {
-    val alphabetCharacter = TMAlphabet[TMRXCounter%TMAlphabet.length].toString().repeat(3)
+// gives its own prefix
+fun <T> Observable<T>.loga(msgPrefix: String? = null, bType: Boolean = false): Observable<T> {
+    val alphabetCharacter = TMAlphabet[TMRXCounter % TMAlphabet.length].toString().repeat(3)
     TMRXCounter++
-    val tempMsgPrefix = if (msgPrefix==null) "" else "`$msgPrefix"
-    return this.log("$alphabetCharacter$tempMsgPrefix", bType=bType)
+    val tempMsgPrefix = if (msgPrefix == null) "" else "`$msgPrefix"
+    return this.log("$alphabetCharacter$tempMsgPrefix", bType = bType)
+}
+
+@SuppressLint("CheckResult")
+fun <T> Observable<T>.logSubscribe(msgPrefix: String? = null, bType: Boolean = false) {
+    val tempMsgPrefix: String = if (msgPrefix == null) "" else {
+        "$msgPrefix`"
+    }
+    this
+        .subscribe({
+            if (bType) {
+                val typeName = if (it == null) {
+                    "null"
+                } else {
+                    (it as Any)::class.java.simpleName
+                }
+                logz("$tempMsgPrefix$typeName`$it")
+            } else {
+                logz("$tempMsgPrefix$it")
+            }
+        }, {
+            logz("${tempMsgPrefix}Error`$it")
+        })
 }
