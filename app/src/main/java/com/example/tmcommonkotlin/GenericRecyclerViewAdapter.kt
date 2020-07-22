1be6f2c0289c4fd3baeeaca348c6f4477a6c836d
Troy Swanson <2troy2020@gmail.com>
2020-07-21 22:09:14 -0500

    comments

--- app/src/main/java/com/example/tmcommonkotlin/GenericRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/tmcommonkotlin/GenericRecyclerViewAdapter.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-// TODO make non-global
 open class GenericRecyclerViewAdapter(
     var binder: Callbacks,
     var context: Context,
