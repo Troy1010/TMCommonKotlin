24a75121280caad333a64c1eea8900ef1b444617
Troy <troye668@gmail.com>
2020-11-28 16:53:30 -0600

    ObservableExtensions`toBehaviorSubject has replay functionality

---
+++ tmcommonkotlin-rx/src/test/java/com/tminus1010/tmcommonkotlin/rx/ObservableExtensionsKtTest.kt
@@ -0,0 +1,31 @@
+package com.tminus1010.tmcommonkotlin.rx
+
+import com.tminus1010.tmcommonkotlin_rx.toBehaviorSubject
+import io.reactivex.rxjava3.core.Observable
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ObservableExtensionsKtTest {
+
+    @Test
+    fun toBehaviorSubject() {
+        // # Given
+        val observable = Observable.just(1,2,3)
+        // # Stimulate
+        val bs = observable
+            .toBehaviorSubject()
+        // # Verify
+        assertEquals(3, bs.value)
+    }
+
+    @Test
+    fun toBehaviorSubject_Overload1() {
+        // # Given
+        val observable = Observable.empty<Int>()
+        // # Stimulate
+        val bs = observable
+            .toBehaviorSubject(6)
+        // # Verify
+        assertEquals(6, bs.value)
+    }
+}
