4eed00de6e08b652a414bc103c927bca1017b3ff
Troy <troye668@gmail.com>
2021-02-23 23:00:21 -0600

    Tuple`Octuple

--- tmcommonkotlin-tuple/src/main/java/com/tminus1010/tmcommonkotlin/tuple/Tuple.kt
+++ tmcommonkotlin-tuple/src/main/java/com/tminus1010/tmcommonkotlin/tuple/Tuple.kt
@@ -60,3 +60,17 @@ data class Septuple<out A, out B, out C, out D, out E, out F, out G>(
     override fun toString(): String = "($first, $second, $third, $fourth, $fifth, $sixth, $seventh)"
 }
 fun <T> Septuple<T, T, T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth, sixth, seventh)
+
+data class Octuple<out A, out B, out C, out D, out E, out F, out G, out H>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D,
+    val fifth: E,
+    val sixth: F,
+    val seventh: G,
+    val eighth: H,
+) : Serializable {
+    override fun toString(): String = "($first, $second, $third, $fourth, $fifth, $sixth, $seventh, $eighth)"
+}
+fun <T> Octuple<T, T, T, T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth, sixth, seventh, eighth)
