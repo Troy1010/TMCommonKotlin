3dd6130c2429c8b552f3581b65bf78a1f0f4f821
Troy <troye668@gmail.com>
2021-02-24 15:36:22 -0600

    TMLog refactored to logz and logx

--- tmcommonkotlin-logz/src/test/java/com/tminus1010/tmcommonkotlin/logz/TMLogKtTest.kt
+++ tmcommonkotlin-logz/src/test/java/com/tminus1010/tmcommonkotlin/logz/TMLogKtTest.kt
@@ -1,10 +1,15 @@
 package com.tminus1010.tmcommonkotlin.logz
 
+import io.reactivex.rxjava3.core.Observable
 import junit.framework.TestCase
 
 class TMLogKtTest : TestCase() {
     fun testLogz() {
-        logx("AAA")
-        "BBB".logx("bbb")
+        logz("AAA")
+        "qwer".logx("BBB")
+        Observable.just("LOGZ").logx("CCC").subscribe()
+
+        logz(Exception("It's error time"))
+        Exception("It's error time").logx("EEE")
     }
 }
--- tmcommonkotlin-rx/src/main/java/com/tminus1010/tmcommonkotlin/rx/extensions/Logz.kt
+++
@@ -1,8 +0,0 @@
-package com.tminus1010.tmcommonkotlin.rx.extensions
-
-import com.tminus1010.tmcommonkotlin.logz.logx
-import io.reactivex.rxjava3.core.Observable
-
-
-fun <T> Observable<T>.logz(msg:Any?) =
-    this.doOnNext { it.logx(msg) }
