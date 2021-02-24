89fef0bcee10cc41dfc878308234155f63835ae9
Troy <troye668@gmail.com>
2021-02-24 01:47:33 -0600

    Misc:gradle`upgrades, removed constraintLayout, comments

--- tmcommonkotlin-misc/build.gradle.kts
+++ tmcommonkotlin-misc/build.gradle.kts
@@ -37,27 +37,26 @@ android {
 }
 
 dependencies {
-    // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.61")
-    //
-    implementation("androidx.core:core-ktx:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
-    // Coroutines
+    // # Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.20")
+    // # Android
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    // # Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.7")
-    // Rx
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    // # Rx
     implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
-    implementation("io.reactivex.rxjava3:rxjava:3.0.4")
-    // Rx+LiveData
-    implementation("androidx.lifecycle:lifecycle-reactivestreams:2.2.0")
-    // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.0")
-    // JUnit
-    testImplementation("junit:junit:4.13")
+    implementation("io.reactivex.rxjava3:rxjava:3.0.7")
+    // ## Rx+LiveData
+    implementation("androidx.lifecycle:lifecycle-reactivestreams:2.3.0")
+    // # Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.3")
+    // # JUnit
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    // Espresso
+    // # Espresso
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
 
