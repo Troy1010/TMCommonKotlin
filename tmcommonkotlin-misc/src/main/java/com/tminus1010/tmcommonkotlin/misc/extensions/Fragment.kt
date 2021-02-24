de8316e1fbcabc6134a4b5804e22c373a3e1313c
Troy <troye668@gmail.com>
2021-02-24 01:30:28 -0600

    Misc:Fragment`viewModels2, nav, v

--- tmcommonkotlin-misc/src/main/java/com/tminus1010/tmcommonkotlin/misc/extensions/Fragment.kt
+++ tmcommonkotlin-misc/src/main/java/com/tminus1010/tmcommonkotlin/misc/extensions/Fragment.kt
@@ -2,7 +2,26 @@ package com.tminus1010.tmcommonkotlin.misc.extensions
 
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
+import com.tminus1010.tmcommonkotlin.misc.createVmFactory
 
-fun Fragment.toast(msg: String, lengthID:Int= Toast.LENGTH_SHORT) {
-    Toast.makeText(requireActivity(), msg, lengthID).show()
+fun Fragment.toast(msg: String, lengthID: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(requireContext(), msg, lengthID).show()
 }
+
+inline fun <reified VM : ViewModel> Fragment.activityViewModels2(noinline function: () -> VM): Lazy<VM> {
+    return this.activityViewModels { createVmFactory(function) }
+}
+
+inline fun <reified VM : ViewModel> Fragment.viewModels2(noinline function: () -> VM): Lazy<VM> {
+    return this.viewModels { createVmFactory(function) }
+}
+
+inline val Fragment.nav
+    get() = findNavController()
+
+inline val Fragment.v
+    get() = requireView()
