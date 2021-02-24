80bacf7790e503f9c738186db434a5a642a08665
Troy <troye668@gmail.com>
2021-02-24 01:31:14 -0600

    Misc:String`isAllDigits simplified

--- tmcommonkotlin-misc/src/main/java/com/tminus1010/tmcommonkotlin/misc/extensions/String.kt
+++ tmcommonkotlin-misc/src/main/java/com/tminus1010/tmcommonkotlin/misc/extensions/String.kt
@@ -1,15 +1,8 @@
 package com.tminus1010.tmcommonkotlin.misc.extensions
 
 
-fun String.isAllDigits(): Boolean {
-    for (ch in this) {
-        if (!ch.isDigit()) {
-            return false
-        }
-    }
-    return true
-}
-fun String.noDoubleSpaces():String {
+fun String.isAllDigits() = this.matches(Regex("^[0-9]*\$"))
+fun String.noDoubleSpaces(): String {
     return if (this.contains("  ")) {
         this.replace("  ", " ").noDoubleSpaces()
     } else {
