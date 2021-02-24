df8d11d3c7e87a68bb02c10c965851c631a34034
Troy <troye668@gmail.com>
2021-02-23 23:18:57 -0600

    gradle & gradle+android upgrades

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.0.0")
+    implementation("com.android.tools.build:gradle:4.1.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
     implementation(gradleApi())
     implementation(localGroovy())
