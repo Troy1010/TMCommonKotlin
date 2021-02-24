cd64c8f333cab3331301bcc8afac27ce9fc9167a
Troy <troye668@gmail.com>
2021-02-24 00:17:49 -0600

    rx:gradle`kotlin,junit upgrades

--- tmcommonkotlin-rx/build.gradle.kts
+++ tmcommonkotlin-rx/build.gradle.kts
@@ -37,9 +37,9 @@ android {
 
 dependencies {
     // # Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.61")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.20")
     // # JUnit
-    testImplementation("junit:junit:4.13")
+    testImplementation("junit:junit:4.13.2")
     // # Rx
     implementation("io.reactivex.rxjava3:rxjava:3.0.7")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
