cc2a18d8be3bdd9bc9b9e0da4c40aa0ac25feba4
Troy <troye668@gmail.com>
2021-02-24 01:59:31 -0600

    Misc:CompositeDisposable plusAssign, Disposable disposeWith

---
+++ tmcommonkotlin-rx/src/main/java/com/tminus1010/tmcommonkotlin/rx/extensions/Disposable.kt
@@ -0,0 +1,8 @@
+package com.tminus1010.tmcommonkotlin.rx.extensions
+
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+fun Disposable.disposeWith(vararg compositeDisposables: CompositeDisposable) {
+    compositeDisposables.forEach { it.add(this) }
+}
