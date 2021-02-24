d962432c2039dcd663dde70f43980b07db1f684a
Troy <troye668@gmail.com>
2021-02-24 00:23:11 -0600

    startWith, endWith, emitIfTimeout

--- tmcommonkotlin-rx/src/main/java/com/tminus1010/tmcommonkotlin/rx/extensions/Misc.kt
+++ tmcommonkotlin-rx/src/main/java/com/tminus1010/tmcommonkotlin/rx/extensions/Misc.kt
@@ -1,6 +1,7 @@
 package com.tminus1010.tmcommonkotlin.rx.extensions
 
 import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 fun <T : Any> Observable<T>.pairwise(initialValue: T) = this.startWithItem(initialValue).pairwise()
 
@@ -14,3 +15,14 @@ fun <T : Any> Observable<T>.pairwise(): Observable<Pair<T, T>> {
 }
 
 fun <T> Observable<T>.noEnd() = this.mergeWith(Observable.never())
+fun <T> Observable<T>.endWith(item: T) = this.concatWith(Observable.just(item))
+fun <T> Observable<T>.startWith(item: T) = this.startWithItem(item)
+
+fun <IN, OUT> Observable<IN>.emitIfTimeout(timespan: Long, timeUnit: TimeUnit, item: OUT): Observable<OUT> {
+    return this
+        .take(1)
+        .map { false }
+        .timeout(timespan, timeUnit, Observable.just(true))
+        .filter { it }
+        .map { item }
+}
