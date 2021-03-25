db6a2c6aaa5fd9f1e6d5e76e692c04be86ae5e00
Troy <troyswanson1010@gmail.com>
2021-03-25 00:33:25 -0500

    version 38

--- buildSrc/src/main/java/com/tminus1010/tmcommonkotlin/Misc.kt
+++ buildSrc/src/main/java/com/tminus1010/tmcommonkotlin/Misc.kt
@@ -1,7 +1,7 @@
 package com.tminus1010.tmcommonkotlin
 
 object Misc {
-    const val versionStr = "1.0.37"
+    const val versionStr = "1.0.38"
     const val groupId = "com.tminus1010.tmcommonkotlin"
     const val targetSDK = 29
     const val minSDK = 22
