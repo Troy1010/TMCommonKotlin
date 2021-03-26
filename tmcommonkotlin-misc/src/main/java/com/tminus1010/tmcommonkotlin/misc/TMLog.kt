41aa1dc87f8624554ca4b901b01499b3e6cf43f3
Troy Swanson <troy.evans@gm.com>
2021-03-25 20:46:14 -0500

    TMLog partially moved to core

--- tmcommonkotlin-misc/src/main/java/com/tminus1010/tmcommonkotlin/misc/TMLog.kt
+++ tmcommonkotlin-misc/src/main/java/com/tminus1010/tmcommonkotlin/misc/TMLog.kt
@@ -1,29 +1,9 @@
 package com.tminus1010.tmcommonkotlin.misc
 
-import android.util.Log
+import com.tminus1010.tmcommonkotlin.core.logx
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
-
-const val TAG = "TMLog"
-
-fun logz(any: Any?, e: Throwable? = null) {
-    when (any) {
-        is Throwable -> Log.e(TAG, "TM`Error:", any)
-        else -> Log.d(TAG, "TM`$any", e)
-    }
-}
-
-inline fun <reified T> T.logx(prefix: Any? = null): T {
-    val logWithPrefix = { any:Any? ->
-        val prefixLogStr = prefix?.let { "$prefix`" } ?: ""
-        when (any) {
-            is Throwable -> Log.e(TAG, "TM`${prefixLogStr}Error:", any)
-            else -> Log.d(TAG, "TM`${prefixLogStr}$any")
-        }
-    }
-    return this.apply { logWithPrefix(this) }
-}
 
 inline fun <reified T> Observable<T>.doLogx(prefix: Any? = null): Observable<T> =
     this.doOnNext { it.logx(prefix) }.doOnComplete { "Completed".logx(prefix) }.doOnError { it.logx(prefix) }
