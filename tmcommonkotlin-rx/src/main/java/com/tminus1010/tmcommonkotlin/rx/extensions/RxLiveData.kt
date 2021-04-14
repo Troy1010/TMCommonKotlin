9a518a27f1ab3af1ba9a9841f60d780310db86fe
Troy <troyswanson1010@gmail.com>
2021-04-14 17:39:27 -0500

    observe() is on mainThread()

--- tmcommonkotlin-rx/src/main/java/com/tminus1010/tmcommonkotlin/rx/extensions/RxLiveData.kt
+++ tmcommonkotlin-rx/src/main/java/com/tminus1010/tmcommonkotlin/rx/extensions/RxLiveData.kt
@@ -2,6 +2,7 @@ package com.tminus1010.tmcommonkotlin.rx.extensions
 
 import androidx.lifecycle.LifecycleOwner
 import com.trello.rxlifecycle4.android.lifecycle.kotlin.bindToLifecycle
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
@@ -11,45 +12,40 @@ import io.reactivex.rxjava3.internal.functions.Functions
 import io.reactivex.rxjava3.internal.observers.LambdaObserver
 
 /**
- * Using observe instead of subscribe makes observables lifecycle-aware.. which means
- * their subscriptions are automatically disposed when the lifecycle owner (ie activity)
- * is destroyed.
+ * These convenience functions make Rx work similar to LiveData
  *
  * Be careful when using with fragments.
  * Most of the time, you will want to use the fragment's viewLifecycleOwner as the lifecycleOwner.
  * Using the fragment itself as the lifecycleOwner is usually not what you want.
  */
-// *Not using default values allows for last-lambda syntax.
 fun <T> Observable<T>.observe(
-    lifecycleOwner: LifecycleOwner,
+    lifecycle: LifecycleOwner,
     onNext: (T) -> Unit
-): Disposable {
-    return LambdaObserver(Consumer(onNext), Functions.ON_ERROR_MISSING, Functions.EMPTY_ACTION, Functions.emptyConsumer())
-        .also { this.bindToLifecycle(lifecycleOwner).subscribe(it) }
-}
+): Disposable =
+    observeOn(AndroidSchedulers.mainThread()).bindToLifecycle(lifecycle)
+        .subscribe { onNext(it) }
 
 fun <T> Observable<T>.observe(
-    lifecycleOwner: LifecycleOwner,
+    lifecycle: LifecycleOwner,
     onNext: (T) -> Unit,
     onError: (Throwable) -> Unit
-): Disposable {
-    return LambdaObserver(Consumer(onNext), Consumer(onError), Functions.EMPTY_ACTION, Functions.emptyConsumer())
-        .also { this.bindToLifecycle(lifecycleOwner).subscribe(it) }
-}
+): Disposable =
+    observeOn(AndroidSchedulers.mainThread()).bindToLifecycle(lifecycle)
+        .subscribe({ onNext(it) }, { onError(it) })
 
 fun <T> Observable<T>.observe(
-    lifecycleOwner: LifecycleOwner,
+    lifecycle: LifecycleOwner,
     onNext: (T) -> Unit,
     onError: (Throwable) -> Unit,
     onComplete: () -> Unit
-): Disposable {
-    return LambdaObserver(Consumer(onNext), Consumer(onError), Action(onComplete), Functions.emptyConsumer())
-        .also { this.bindToLifecycle(lifecycleOwner).subscribe(it) }
-}
+): Disposable =
+    observeOn(AndroidSchedulers.mainThread()).bindToLifecycle(lifecycle)
+        .subscribe({ onNext(it) }, { onError(it) }, { onComplete() })
 
 fun <T> Observable<T>.observe(
-    lifecycleOwner: LifecycleOwner,
+    lifecycle: LifecycleOwner,
     observer: Observer<T>
 ) {
-    this.bindToLifecycle(lifecycleOwner).subscribe(observer)
+    observeOn(AndroidSchedulers.mainThread()).bindToLifecycle(lifecycle)
+        .subscribe(observer)
 }
