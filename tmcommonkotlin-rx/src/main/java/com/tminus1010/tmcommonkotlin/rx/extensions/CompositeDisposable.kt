a8e9d0c476334122f81ca0c73eccf37c9a71ec9b
Troy <troyswanson1010@gmail.com>
2021-05-23 17:55:56 -0500

    deprecation

--- tmcommonkotlin-rx/src/main/java/com/tminus1010/tmcommonkotlin/rx/extensions/CompositeDisposable.kt
+++ tmcommonkotlin-rx/src/main/java/com/tminus1010/tmcommonkotlin/rx/extensions/CompositeDisposable.kt
@@ -3,6 +3,7 @@ package com.tminus1010.tmcommonkotlin.rx.extensions
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 
+@Deprecated("use RxKotlin")
 operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
     this.add(disposable)
 }
