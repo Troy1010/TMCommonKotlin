9d2197ea618763957f512adb6640052ebeaaf852
Troy <troyswanson1010@gmail.com>
2021-06-27 01:48:31 -0500

    logz can handle test/

--- tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/TMLog.kt
+++ tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/TMLog.kt
@@ -5,19 +5,36 @@ import android.util.Log
 const val TAG = "TMLog"
 
 fun logz(any: Any?, e: Throwable? = null) {
-    when (any) {
-        is Throwable -> Log.e(TAG, "TM`Error:", any)
-        else -> Log.d(TAG, "TM`$any", e)
+    try {
+        when (any) {
+            is Throwable -> Log.e(TAG, "TM`Error:", any)
+            else -> Log.d(TAG, "TM`$any", e)
+        }
+    } catch (e2: java.lang.RuntimeException) {
+        if (e2.message?.matches(Regex(""".*in android.util.Log not mocked.*""")) ?: false)
+            when (any) {
+                is Throwable -> println("TM`Error:${any.message}")
+                else -> println("TM`$any${e?.message?.let { " $it" } ?: ""}")
+            }
+        else throw e2
     }
 }
 
 inline fun <reified T> T.logx(prefix: Any? = null): T {
-    val logWithPrefix = { any:Any? ->
+    return this.apply {
         val prefixLogStr = prefix?.let { "$prefix`" } ?: ""
-        when (any) {
-            is Throwable -> Log.e(TAG, "TM`${prefixLogStr}Error:", any)
-            else -> Log.d(TAG, "TM`${prefixLogStr}$any")
+        try {
+            when (this) {
+                is Throwable -> Log.e(TAG, "TM`${prefixLogStr}Error:", this)
+                else -> Log.d(TAG, "TM`${prefixLogStr}$this")
+            }
+        } catch (e2: java.lang.RuntimeException) {
+            if (e2.message?.matches(Regex(""".*in android.util.Log not mocked.*""")) ?: false)
+                when (this) {
+                    is Throwable -> println("TM`Error:${this.message}")
+                    else -> println("TM`${prefixLogStr}$this")
+                }
+            else throw e2
         }
     }
-    return this.apply { logWithPrefix(this) }
 }
