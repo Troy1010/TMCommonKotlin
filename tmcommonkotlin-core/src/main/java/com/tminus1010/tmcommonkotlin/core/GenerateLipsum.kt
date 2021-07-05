a543c2df9ded5a13506d93d057edf773d2d54646
Troy <troyswanson1010@gmail.com>
2021-07-05 03:43:32 -0500

    GenerateLipsum simplified

--- tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/GenerateLipsum.kt
+++ tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/GenerateLipsum.kt
@@ -3,16 +3,8 @@ package com.tminus1010.tmcommonkotlin.core
 
 fun generateLipsum(size: Int): List<String> {
     val alphabet = "abcdefghijklmnopqrstuvwxyz"
-    val returning = ArrayList<String>()
-    for (i in 0 until size) {
-        var s = ""
-        val sizeOfWord = (4..30).random()
-        for (j in 0 until sizeOfWord) {
-            s += alphabet.random()
-        }
-        returning.add(s)
-    }
-    return returning.toList()
+    return (0 until size)
+        .map { (0 until (4..30).random()).map { alphabet.random() }.joinToString("") }
 }
 
 fun generateLipsum(): String {
