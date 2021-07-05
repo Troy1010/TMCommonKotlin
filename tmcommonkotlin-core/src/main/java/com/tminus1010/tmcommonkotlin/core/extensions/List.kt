2d57873de4d07751856040824e0f89a367b6ce11
Troy <troyswanson1010@gmail.com>
2021-07-05 15:42:47 -0500

    renaming

--- tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/extensions/List.kt
+++ tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/extensions/List.kt
@@ -1,9 +1,9 @@
 package com.tminus1010.tmcommonkotlin.core.extensions
 
 fun <T> List<List<T>>.reflectXY(): List<List<T>> {
-    return this[0].indices.map { x ->
-        this.indices.map { y ->
-            this[y][x]
+    return this[0].indices.map { y ->
+        this.indices.map { x ->
+            this[x][y]
         }
     }
 }
