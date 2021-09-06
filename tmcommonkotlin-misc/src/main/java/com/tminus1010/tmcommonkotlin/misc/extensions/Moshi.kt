81933956ab5cd66ded0b4f9d0938e02c8c24f487
Troy <troyswanson1010@gmail.com>
2021-09-06 00:52:07 -0500

    Moshi`nullable output overload

--- tmcommonkotlin-misc/src/main/java/com/tminus1010/tmcommonkotlin/misc/extensions/Moshi.kt
+++ tmcommonkotlin-misc/src/main/java/com/tminus1010/tmcommonkotlin/misc/extensions/Moshi.kt
@@ -5,5 +5,13 @@ import com.squareup.moshi.Moshi
 inline fun <reified T> Moshi.toJson(x: T): String =
     this.adapter(T::class.java).toJson(x)
 
-inline fun <reified T> Moshi.fromJson(s: String): T =
-    this.adapter(T::class.java).fromJson(s)!!
+inline fun <reified T : Any> Moshi.fromJson(s: String): T {
+    if (s == "null") error("string was \"null\". If you are expecting nulls - even \"null\" strings - then make the input string nullable.")
+    return this.adapter(T::class.java).fromJson(s) ?: error("s:$s produced null result")
+}
+
+@JvmName("fromJsonNullable")
+inline fun <reified T> Moshi.fromJson(s: String?): T? {
+    if (s == null) return null
+    return this.adapter(T::class.java).fromJson(s)
+}
