8bd58ed1453f31ff9dd6369652619db9c1053279
Troy <troyswanson1010@gmail.com>
2021-09-26 14:17:18 -0500

    Exhaustive

---
+++ tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/extensions/T.kt
@@ -0,0 +1,13 @@
+package com.tminus1010.tmcommonkotlin.core.extensions
+
+/**
+ * Useful for when() blocks that you want to be exhaustive, to cause compile-time errors.
+ * Example usage:
+ *      when(someEnum) {
+ *          SomeEnum.A -> 1
+ *          SomeEnum.B -> 24
+ *          SomeEnum.C -> 486
+ *      }.exhaustive
+ */
+val <T> T.exhaustive: T
+    get() = this
