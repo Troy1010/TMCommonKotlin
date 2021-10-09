ab87cedd2d2c1cc96da3776f0a78975f04297f63
Troy <troyswanson1010@gmail.com>
2021-10-09 02:22:56 -0500

    "Probably was not a 2d grid" exception

--- tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/extensions/List.kt
+++ tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/extensions/List.kt
@@ -1,9 +1,13 @@
 package com.tminus1010.tmcommonkotlin.core.extensions
 
 fun <T> List<List<T>>.reflectXY(): List<List<T>> {
-    return this[0].indices.map { y ->
-        this.indices.map { x ->
-            this[x][y]
+    return try {
+        this[0].indices.map { y ->
+            this.indices.map { x ->
+                this[x][y]
+            }
         }
+    } catch (e: IndexOutOfBoundsException) {
+        throw Exception("Probably was not a 2d grid", e)
     }
 }
