7c3eadbcbdeda38423e4f857077c2d14ecc0b066
Troy <troyswanson1010@gmail.com>
2022-04-02 17:50:38 -0500

    easyCleanPublishLocalForAll fixed after adding exampleclient

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,24 +8,26 @@ allprojects {
 }
 
 subprojects {
-    tasks.register("easyPublishLocal") {
-        group = "publishing"
-        dependsOn("assemble")
-        finalizedBy("publishToMavenLocal")
-    }
-    tasks.register("easyCleanPublishLocal") {
-        group = "publishing"
-        dependsOn("clean")
-        finalizedBy("easyPublishLocal")
-        description = "Do not use in main project"
-    }
+    if (tasks.names.contains("publishToMavenLocal"))
+        tasks.register("easyPublishLocal") {
+            group = "publishing"
+            dependsOn("assemble")
+            finalizedBy("publishToMavenLocal")
+        }
+    if (tasks.names.contains("easyPublishLocal"))
+        tasks.register("easyCleanPublishLocal") {
+            group = "publishing"
+            dependsOn("clean")
+            finalizedBy("easyPublishLocal")
+            description = "Do not use in main project"
+        }
 }
 // # easyCleanPublishLocal will stall forever if run by root project
 tasks.register("easyCleanPublishLocal") { enabled = false }
 tasks.register("easyCleanPublishLocalForAll") {
     group = "publishing"
     val allCleanTasks = project.getTasksByName("clean", true)
-    val allAssemblePublishTasks = subprojects.map { it.tasks.getByName("easyPublishLocal") }
+    val allAssemblePublishTasks = subprojects.mapNotNull { kotlin.runCatching { it.tasks.getByName("easyPublishLocal") }.getOrNull() }
     dependsOn(allCleanTasks, allAssemblePublishTasks)
     allAssemblePublishTasks.forEach { it.mustRunAfter(allCleanTasks) }
 }
