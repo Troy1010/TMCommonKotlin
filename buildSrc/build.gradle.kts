eba6274dbbc3f4021eee94b0869d115d09c7dda8
Troy <troyswanson1010@gmail.com>
2022-04-16 00:35:36 -0500

    gradle`upgrade kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:4.1.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20") // migration to 1.4.31 gave warning: Unsupported Kotlin Version
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10") // migration to 1.4.31 gave warning: Unsupported Kotlin Version
     implementation(gradleApi())
     implementation(localGroovy())
 }
