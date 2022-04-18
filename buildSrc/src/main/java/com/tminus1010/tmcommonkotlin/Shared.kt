3b6181da1ce3841137775bbc7b27fd59636ca96f
Troy <troyswanson1010@gmail.com>
2022-04-18 04:31:54 -0500

    versionName 1.2.0 versionCode 3

--- buildSrc/src/main/java/com/tminus1010/tmcommonkotlin/Shared.kt
+++ buildSrc/src/main/java/com/tminus1010/tmcommonkotlin/Shared.kt
@@ -1,8 +1,8 @@
 package com.tminus1010.tmcommonkotlin
 
 object Shared {
-    const val versionName = "1.1.0"
-    const val versionCode = 2
+    const val versionName = "1.2.0"
+    const val versionCode = 3
     const val groupId = "com.tminus1010.tmcommonkotlin"
     const val targetSDK = 31
     const val minSDK = 22
