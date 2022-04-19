3bc324937bf2dc47b0faa49bf7b7d6561075887d
Troy <troyswanson1010@gmail.com>
2022-04-18 20:10:17 -0500

    ShowToast and ShowAlertDialog

---
+++ tmcommonkotlin-androidx/src/main/java/com/tminus1010/tmcommonkotlin/androidx/ShowAlertDialog.kt
@@ -0,0 +1,59 @@
+package com.tminus1010.tmcommonkotlin.androidx
+
+import android.app.Activity
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.tminus1010.tmcommonkotlin.view.NativeText
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+/**
+ * Must use [Activity] or you will see: java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity.
+ */
+class ShowAlertDialog constructor(private val activity: Activity) {
+    /**
+     * EditText
+     */
+    suspend operator fun invoke(body: NativeText, initialText: CharSequence? = null, onSubmitText: ((CharSequence?) -> Unit)? = null, onNo: (() -> Unit)? = null) = suspendCoroutine<Unit> { downstream ->
+        launchOnMainThread {
+            val editText = EditText(activity)
+            editText.setText(initialText)
+            AlertDialog.Builder(activity)
+                .setMessage(body.toCharSequence(activity))
+                .setView(editText)
+                .setPositiveButton("Submit") { _, _ -> onSubmitText?.invoke(editText.text) }
+                .setNegativeButton("Cancel") { _, _ -> onNo?.invoke() }
+                .setOnDismissListener { downstream.resume(Unit) }
+                .show()
+        }
+    }
+
+
+    /**
+     * Yes/no
+     */
+    suspend operator fun invoke(body: NativeText, onYes: (() -> Unit)? = null, onNo: (() -> Unit)? = null) = suspendCoroutine<Unit> { downstream ->
+        launchOnMainThread {
+            AlertDialog.Builder(activity)
+                .setMessage(body.toCharSequence(activity))
+                .setPositiveButton("Yes") { _, _ -> onYes?.invoke() }
+                .setNegativeButton("No") { _, _ -> onNo?.invoke() }
+                .setOnDismissListener { downstream.resume(Unit) }
+                .show()
+        }
+    }
+
+
+    /**
+     * Okay
+     */
+    suspend operator fun invoke(body: NativeText) = suspendCoroutine<Unit> { downstream ->
+        launchOnMainThread {
+            AlertDialog.Builder(activity)
+                .setMessage(body.toCharSequence(activity))
+                .setPositiveButton("Okay") { _, _ -> }
+                .setOnDismissListener { downstream.resume(Unit) }
+                .show()
+        }
+    }
+}
