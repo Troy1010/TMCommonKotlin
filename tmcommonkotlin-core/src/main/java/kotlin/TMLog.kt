8681fa80ad8a60ffe56f006a30bb4c97da8d9111
Troy <troyswanson1010@gmail.com>
2022-06-10 18:51:12 -0500

    TMLog bugfix - println code was not running in JUnit

--- tmcommonkotlin-core/src/main/java/kotlin/TMLog.kt
+++ tmcommonkotlin-core/src/main/java/kotlin/TMLog.kt
@@ -2,39 +2,34 @@ package kotlin
 // ^ packages that start with "kotlin" are automatically imported
 
 import android.util.Log
+import com.tminus1010.tmcommonkotlin.core.isJUnitTest
 
 fun logz(any: Any?, e: Throwable? = null) {
-    try {
+    if (isJUnitTest)
         when (any) {
             is Throwable -> Log.e("TMLog", "TM`Error:", any)
             else -> Log.d("TMLog", "TM`$any", e)
         }
-    } catch (e2: Throwable) {
-        if (e2.message?.matches(Regex(""".*android.util.Log.*""")) ?: false)
-            when (any) {
-                is Throwable -> println("TM`Error:${any.message}")
-                else -> println("TM`$any${e?.message?.let { " $it" } ?: ""}")
-            }
-        else throw e2
-    }
+    else
+        when (any) {
+            is Throwable -> println("TM`Error:${any.message}")
+            else -> println("TM`$any${e?.message?.let { " $it" } ?: ""}")
+        }
 }
 
 inline fun <reified T> T.logx(prefix: Any? = null): T {
     return this.apply {
         val prefixLogStr = prefix?.let { "$prefix`" } ?: ""
-        try {
+        if (isJUnitTest)
+            when (this) {
+                is Throwable -> println("TM`Error:${this.message}")
+                else -> println("TM`${prefixLogStr}$this")
+            }
+        else
             when (this) {
                 is Throwable -> Log.e("TMLog", "TM`${prefixLogStr}Error:", this)
                 else -> Log.d("TMLog", "TM`${prefixLogStr}$this")
             }
-        } catch (e2: Throwable) {
-            if (e2.message?.matches(Regex(""".*android.util.Log.*""")) ?: false)
-                when (this) {
-                    is Throwable -> println("TM`Error:${this.message}")
-                    else -> println("TM`${prefixLogStr}$this")
-                }
-            else throw e2
-        }
     }
 }
 
