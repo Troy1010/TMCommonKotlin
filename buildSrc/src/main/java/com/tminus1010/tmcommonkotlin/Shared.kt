237c0d013631aab3181861498f33fd453b535bf7
Troy <troyswanson1010@gmail.com>
2022-08-12 01:19:39 -0500

    versionBump

--- buildSrc/src/main/java/com/tminus1010/tmcommonkotlin/Shared.kt
+++ buildSrc/src/main/java/com/tminus1010/tmcommonkotlin/Shared.kt
@@ -1,8 +1,8 @@
 package com.tminus1010.tmcommonkotlin
 
 object Shared {
-    const val versionName = "1.3.0"
-    const val versionCode = 4
+    const val versionName = "1.3.1"
+    const val versionCode = 5
     const val groupId = "com.tminus1010.tmcommonkotlin"
     const val targetSDK = 31
     const val minSDK = 22
