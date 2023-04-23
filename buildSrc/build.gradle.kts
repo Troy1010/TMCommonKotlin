e4f5d6b9cad8571c608af7195be253bca6449bcd
Troy <troyswanson1010@gmail.com>
2023-04-23 13:41:13 -0500

    upgrade kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.1.3")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
     implementation(gradleApi())
     implementation(localGroovy())
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.41")
