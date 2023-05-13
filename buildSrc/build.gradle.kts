d4a1c7b27e9f86e17defaca312432e6700d332d3
Troy <troyswanson1010@gmail.com>
2023-05-13 16:02:20 -0500

    downgrade buildSrc gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.0.1")
+    implementation("com.android.tools.build:gradle:7.3.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
     implementation(gradleApi())
     implementation(localGroovy())
