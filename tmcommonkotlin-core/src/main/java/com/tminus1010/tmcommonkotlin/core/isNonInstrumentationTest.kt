4dc4b6e137aadf2c7300705f89f1b7d3dbf51f12
Troy <troyswanson1010@gmail.com>
2023-05-13 00:38:20 -0500

    not using isNonInstrumentationTest in logz

--- tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/isNonInstrumentationTest.kt
+++ tmcommonkotlin-core/src/main/java/com/tminus1010/tmcommonkotlin/core/isNonInstrumentationTest.kt
@@ -1,5 +1,8 @@
 package com.tminus1010.tmcommonkotlin.core
 
+/**
+ * This is not 100% reliable.
+ */
 val isNonInstrumentationTest by lazy {
     Thread.currentThread().stackTrace.any { it.className.startsWith("org.junit.") }
 }
