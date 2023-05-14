6909aba1464790672fc6ec981e6ebe19de03fa51
Troy <troyswanson1010@gmail.com>
2023-05-13 21:26:24 -0500

    logd, logv

--- tmcommonkotlin-core/src/main/java/kotlin/EasyLog.kt
+++ tmcommonkotlin-core/src/main/java/kotlin/EasyLog.kt
@@ -4,25 +4,49 @@ package kotlin
 import android.util.Log
 
 /**
- * This is meant as a convenience method if you don't want to bother specifying a tag or log level.
+ * This is meant as a convenience method if you don't want to bother specifying a tag.
  *
  * The message prefix is meant to help with log filtering.
  * As of 2023_05_13, Android Studio's old log filter did not allow mixing tag and message filters.
  * Perhaps with Android Studio's new log filter, this will no longer be necessary.
  */
-fun logq(any: Any?, e: Throwable? = null) {
+fun logv(any: Any?, e: Throwable? = null) {
     when (any) {
-        is Throwable -> Log.e("LOGQ", "LQ`Error:", any)
-        else -> Log.d("LOGQ", "LQ`$any", e)
+        is Throwable -> Log.v("TAG", "`Error:", any)
+        else -> Log.v("TAG", "`$any", e)
     }
 }
 
-inline fun <reified T> T.logw(prefix: Any? = null): T {
+inline fun <reified T> T.logvx(prefix: Any? = null): T {
     return this.apply {
         val prefixLogStr = prefix?.let { "$prefix`" } ?: ""
         when (this) {
-            is Throwable -> Log.e("LOGQ", "LQ`${prefixLogStr}Error:", this)
-            else -> Log.d("LOGQ", "LQ`${prefixLogStr}$this")
+            is Throwable -> Log.v("TAG", "`${prefixLogStr}Error:", this)
+            else -> Log.v("TAG", "`${prefixLogStr}$this")
+        }
+    }
+}
+
+/**
+ * This is meant as a convenience method if you don't want to bother specifying a tag.
+ *
+ * The message prefix is meant to help with log filtering.
+ * As of 2023_05_13, Android Studio's old log filter did not allow mixing tag and message filters.
+ * Perhaps with Android Studio's new log filter, this will no longer be necessary.
+ */
+fun logd(any: Any?, e: Throwable? = null) {
+    when (any) {
+        is Throwable -> Log.d("TAG", "`Error:", any)
+        else -> Log.d("TAG", "`$any", e)
+    }
+}
+
+inline fun <reified T> T.logdx(prefix: Any? = null): T {
+    return this.apply {
+        val prefixLogStr = prefix?.let { "$prefix`" } ?: ""
+        when (this) {
+            is Throwable -> Log.d("TAG", "`${prefixLogStr}Error:", this)
+            else -> Log.d("TAG", "`${prefixLogStr}$this")
         }
     }
 }
