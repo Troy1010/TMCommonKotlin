1232b2bd09e6d7ae6e201ef1522baa81bd6fea8c
Troy <troyswanson1010@gmail.com>
2023-05-16 01:51:35 -0500

    Shared.java

--- buildSrc/src/main/java/com/tminus1010/tmcommonkotlin/Shared.kt
+++ buildSrc/src/main/java/com/tminus1010/tmcommonkotlin/Shared.kt
@@ -1,7 +1,10 @@
 package com.tminus1010.tmcommonkotlin
 
+import org.gradle.api.JavaVersion
+
 object Shared {
     const val groupId = "com.tminus1010.tmcommonkotlin"
     const val targetSDK = 31
     const val minSDK = 22
+    val java = JavaVersion.VERSION_17
 }
